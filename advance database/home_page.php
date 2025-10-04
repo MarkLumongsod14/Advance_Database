a6b611d7e28a775bd3509011ff724bfb48b12218
MarkLumongsod14 <mark.adrian.lumongsod@gmail.com>
2025-10-04 10:25:39 +0800

    Update home_page.php

--- advance database/home_page.php
+++ advance database/home_page.php
@@ -85,6 +85,7 @@ footer {
       <li><a href="home_page.php?page=london">London</a></li>
       <li><a href="home_page.php?page=paris">Paris</a></li>
       <li><a href="home_page.php?page=tokyo">Tokyo</a></li>
+            <li><a href="home_page.php?page=cdo">Cdo</a></li>
     </ul>
   </nav>
   
@@ -101,6 +102,9 @@ footer {
           break;
         case 'tokyo':
           include 'modules/tokyo.php';
+          break;
+          case 'cdo':
+          include 'modules/cdo.php'; 
           break;
 
       }
